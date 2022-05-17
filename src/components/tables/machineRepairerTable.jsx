19629d05b9f0ee3320c5781687f91dfa9f3dd7ad
Maruf <marufzoiroff@gmail.com>
2022-05-17 09:30:43 +0500

    added machine Repairer feature

---
+++ src/components/tables/machineRepairerTable.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Table from "../common/table";
+
+class MachineRepairerTable extends Component {
+  columns = [
+    { path: "employee.fullName", label: "" },
+    { path: "employee.departmentName", label: "" },
+    { path: "employee.position", label: "" },
+    {
+      path: "delete",
+      content: (repairer) => (
+        <button
+          type="button"
+          onClick={() => this.props.onDelete(repairer)}
+          className="btn btn-danger"
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
+  render() {
+    const { rows, sortColumn, onSort } = this.props;
+    return (
+      <Table
+        columns={this.columns}
+        rows={rows}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+    );
+  }
+}
+
+export default MachineRepairerTable;
